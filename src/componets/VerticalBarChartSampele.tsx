9320e02b38a8374d611ebd0e0eb8cc35454633e5
Chang Hyun Kim <hot9998@naver.com>
2021-11-12 15:46:45 +0900

    Change vertical bar chart's data

--- src/componets/VerticalBarChartSampele.tsx
+++ src/componets/VerticalBarChartSampele.tsx
@@ -19,25 +19,29 @@ const VerticalBarChartSampele = () => {
       (_, index) => `${index + 1}월`
     );
 
+    const colors = [
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+      "rgba(126, 75, 25, 0.2)",
+    ];
+
+    const datasets = Array.from({ length: 4 }).map((_, index) => ({
+      label: `범례${index + 1}`,
+      data: Array.from({ length: 7 }).map((_) =>
+        Math.floor(Math.random() * 100)
+      ),
+      backgroundColor: colors[index],
+    }));
+
     const config: ChartConfiguration = {
       type: "bar",
       data: {
         labels: months,
-        datasets: [
-          {
-            label: "test",
-            data: [35, 23, 53, 12, 35, 23, 65],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-              "rgba(126, 75, 25, 0.2)",
-            ],
-          },
-        ],
+        datasets,
       },
     };
 
